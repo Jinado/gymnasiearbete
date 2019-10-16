02dbdd3de381c40062972bab17871ab36025f989
Johannes Emmoth <johannes.emmoth@gmail.com>
2019-10-16 17:54:01 +0200

    Fixed the file structure as well as added some partials and a basic 404 page

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/public/ejs'));
 
-const f = ["Pandas are white and black", "Lilija is fucking awesome", "Privet means hello in Russian"];
+app.get("/", (req, res) => {
+    res.render("pages/index", {title: "Homepage"});
+});
 
-app.get("/facts", (req, res) => {
-    res.render("facts", { facts: f });
+
+// 404 - Error
+app.get("*", (req, res) => {
+    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    res.render("errors/error404", {url: url, title: "404 - Page not found"});
 });
 
 app.listen(5000, () => console.log("Server listening on port 5000!"));

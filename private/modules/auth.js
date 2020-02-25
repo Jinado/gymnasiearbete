613841a329eb382c34197dfd17581511be9c2677
Johannes Emmoth <johannes.emmoth@gmail.com>
2020-02-25 19:31:10 +0100

    Need to redirect the user to the root page

--- private/modules/auth.js
+++ private/modules/auth.js
@@ -42,27 +42,9 @@ module.exports = {
                 }
             });
         }else{
-            res.send("No token provided!");
+            res.redirect("/");
         }
     },
-    /*loginUser: async credentials =>{
-        // Hämta användarna
-        const users = require("./users");
-
-        // Kollar om användaren finns
-        const user = users.filter(u => u.email === credentials.email);
-
-        if(user.length === 1){
-            // CHECK PASSWORD AGAINST DATABASE
-            const success = await bcrypt.compare(credentials.password, user[0].password);
-            if(success){
-                const token = jwt.sign({email: user[0].email}, secret.jwtSecret, {expiresIn: "7d"});
-                return [token, success];
-            }
-        }
-
-        return ["no token", false];
-    },*/
     verifyAndRetrieve: _verifyAndRetrieve,
     warnedOfCookies: (req, res, next) => {
         if(!req.cookies.warnedOfCookies){
